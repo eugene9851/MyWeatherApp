493814bd8427621e6ca72d036e0a68967faa4cc9
eugene <eugene@radexel.com>
2025-08-14 17:25:13 +0900

    ScrollView and its props doesn't apply flex

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,57 @@
-import { View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
 
 export default function Index() {
   return (
-    <View style={{ flex: 1 }}>
-      <View style={{ backgroundColor: "red", flex: 1 }}></View>
-      <View style={{ backgroundColor: "blue", flex: 1 }}></View>
-      <View style={{ backgroundColor: "green", flex: 1 }}></View>
+    <View style={styles.container}>
+      <View style={styles.city}>
+        <Text style={styles.cityName}>Seoul</Text>
+      </View>
+      <ScrollView horizontal pagingEnabled indicatorStyle="black" contentContainerStyle={styles.weather}>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+        <View style={styles.day}>
+          <Text style={styles.temp}>27</Text>
+          <Text style={styles.description}>Sunny</Text>
+        </View>
+      </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
+  },
+  city: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+  },
+  weather: {
+  },
+  day: {
+    width: SCREEN_WIDTH,
+    alignItems: "center",
+  },
+  temp: {
+    fontSize: 178,
+    marginTop: 50,
+  },
+  description: {
+    fontSize: 60,
+    marginTop: -30,
+  },
+})
